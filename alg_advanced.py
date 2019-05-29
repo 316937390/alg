b9031bf80bb4d09cd85adce118ada9b9b4c42dc1
li.zhang <li.zhang@mobvista.com>
2019-05-29 17:12:01 +0800

    add BFS实现拓扑排序

--- alg_advanced.py
+++ alg_advanced.py
@@ -5,9 +5,9 @@ import Queue
 ##拓扑排序：有向无环图
 ##Kahn实现拓扑排序：借助队列将顶点顺序输出，迭代时将入度为0的顶点入队，某顶点出队时，将其可达顶点的入度都减1，如果顶点入度变为0就入队
 graph = {0:[1,3],2:[4,0,1],1:[3],3:[4],4:[]}
-def topoSort_Kahn():
+def topoSort_Kahn(n):
     #统计每个顶点的入度
-    inDegree = {0:0,1:0,2:0,3:0,4:0}
+    inDegree = {k:0 for k in range(5)}
     for k,v in graph.items():
         for i in v:
             inDegree[i] += 1
@@ -26,7 +26,7 @@ def topoSort_Kahn():
                 q.put(tv)
     print("end")
 
-topoSort_Kahn()
+topoSort_Kahn(5)
 
 ##拓扑排序：有向无环图
 ##DFS实现拓扑排序：首先构造逆邻接表，然后进行深度优先遍历
@@ -47,7 +47,7 @@ def dfs(v,visited,inverse):
     print("->%d" % v,end="")
 
 def topoSort_DFS(num_vertex,inverse):
-    visited = {0:False,1:False,2:False,3:False,4:False}
+    visited = {k:False for k in range(num_vertex)}
     for v in range(num_vertex):
         if not visited[v]:
             visited[v] = True
@@ -55,4 +55,33 @@ def topoSort_DFS(num_vertex,inverse):
     print("")
 
 topoSort_DFS(5,inverse_graph)
+
+##拓扑排序：有向无环图
+##BFS实现拓扑排序：从入度为0的顶点开始，进行BFS
+def topoSort_BFS(n):
+    #统计每个顶点的入度
+    inDegree = {k:0 for k in range(n)}
+    for k,v in graph.items():
+        for i in v:
+            inDegree[i] += 1
+    #记录顶点是否打印
+    visited = {k:False for k in range(n)}
+    #队列用于逐层遍历
+    q = Queue.Queue()
+    for k,v in inDegree.items():
+        if v == 0:
+            q.put(k)
+    while not q.empty():
+        vertex = q.get()
+        if visited[vertex]:
+            continue
+        if len(graph.get(vertex)) == 0 and not q.empty():
+            continue
+        for tv in graph.get(vertex):
+            q.put(tv)
+        visited[vertex] = True
+        print(vertex,end="->")
+    print("end")
+
+topoSort_BFS(5)
 
