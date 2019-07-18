c9d6420e289b489071a04bdb55592334a49a2bbe
li.zhang <li.zhang@mobvista.com>
2019-07-18 16:10:33 +0800

    add sort_1.py 示例代码

--- sort_1.py
+++ sort_1.py
@@ -14,11 +14,65 @@ from __future__ import print_function
 逆序度 = 满有序度 - 有序度
 排序的过程就是增加有序度，减少逆序度，最后达到满有序度。
 
-冒泡排序：冒泡排序只会操作相邻的两个数据，一次冒泡会让至少一个元素移动到它应该在的位置，重复n次，就完成了n个数据的排序工作。
-插入排序：取未排序区间中的元素，在已排序区间中找到合适的位置将其插入，并保证已排序区间数据一直有序，重复这个过程，直到未排序区间中元素为空，排序结束。
-选择排序：
+1.冒泡排序：冒泡排序只会操作相邻的两个数据，一次冒泡会让至少一个元素移动到它应该在的位置，重复n次，就完成了n个数据的排序工作。
+2.插入排序：取未排序区间中的元素，在已排序区间中找到合适的位置将其插入，并保证已排序区间数据一直有序，重复这个过程，直到未排序区间中元素为空，排序结束。
+3.选择排序：每次会从未排序区间中找到最小的元素，将其放到已排序区间的末尾。
 """
+
+##
+def bubble_sort(array_input):
+    for i in range(len(array_input)):
+        swap_flag  = False
+        for j in range(len(array_input)-i-1):
+            if array_input[j] > array_input[j+1]:
+                tmp = array_input[j+1]
+                array_input[j+1] = array_input[j]
+                array_input[j] = tmp
+                swap_flag = True
+        if not swap_flag:
+            break
+
+
+##
+def insert_sort(array_input):
+    for i in range(1,len(array_input),1):
+        value = array_input[i]
+        j = i - 1
+        while j >= 0:
+            if value < array_input[j]:
+                array_input[j+1] = array_input[j]
+                j -= 1
+            else:
+                break
+        array_input[j+1] = value
+
+##
+def select_sort(array_input):
+    for i in range(len(array_input)):
+        min_v = array_input[i]
+        idx = i
+        flag = False
+        for j in range(i,len(array_input),1):
+            if array_input[j] < min_v:
+                min_v = array_input[j]
+                idx = j
+                flag = True
+        if flag:
+            tmp = array_input[i]
+            array_input[i] = array_input[idx]
+            array_input[idx] = tmp
 
 
 if __name__ == "__main__":
-    pass
+    ##测试冒泡排序
+    arr_1 = [4,5,3,1,8,8,2]
+    bubble_sort(arr_1)
+    print(arr_1)
+    ##测试插入排序
+    arr_2 = [1,1,7,2,8,6,5,4,2]
+    insert_sort(arr_2)
+    print(arr_2)
+    ##测试选择排序
+    arr_3 = [5,8,5,3,2,7,6]
+    select_sort(arr_3)
+    print(arr_3)
