d133fcd54b735123718dd49accbfb19135df7c0b
li.zhang <li.zhang@mobvista.com>
2019-08-19 14:16:42 +0800

    add heap.removeMax()

--- BinaryTree/heap.py
+++ BinaryTree/heap.py
@@ -10,9 +10,12 @@ from __future__ import print_function
 如何实现一个堆：
     存储一个堆：数组
     往堆中插入一个元素：堆化（heapify）
-    删除堆顶元素：
+    删除堆顶元素：从上往下堆化
+    堆化过程是顺着节点所在路径比较交换，堆化的时间复杂度跟树的高度成正比，即O(logn)。
+    上述插入、删除的主要逻辑就是堆化，因此往堆中插入一个元素和删除堆顶元素的时间复杂度都是O(logn)。
+
 """
-##
+##大顶堆
 class Heap(object):
     def __init__(self,capacity):
         self.data = [None]*(capacity+1)
@@ -25,12 +28,36 @@ class Heap(object):
         self.count += 1
         self.data[self.count] = value
         i = self.count
+        ##堆化
         while (i>>1) > 0 and self.data[i] > self.data[(i>>1)]:
             tmp = self.data[(i>>1)]
             self.data[(i>>1)] = self.data[i]
             self.data[i] = tmp
             i = (i>>1)
         return True
+
+    def removeMax(self):
+        if self.count == 0:
+            return False
+        self.data[1] = self.data[self.count]
+        self.count -= 1
+        ##堆化
+        self.heapify(1)
+        return True
+
+    def heapify(self,i):
+        while True:
+            maxPos = i
+            if i*2 <= self.count and self.data[i] < self.data[i*2]:
+                maxPos = i*2
+            if i*2+1 <= self.count and self.data[maxPos] < self.data[i*2+1]:
+                maxPos = i*2+1
+            if maxPos == i:
+                break
+            tmp = self.data[i]
+            self.data[i] = self.data[maxPos]
+            self.data[maxPos] = tmp
+            i = maxPos
 
 
 
@@ -39,4 +66,6 @@ if __name__ == "__main__":
     heap = Heap(20)
     for i in range(25):
         heap.insert(i)
-    print(heap.data)
+    print(heap.data,heap.count)
+    heap.removeMax()
+    print(heap.data,heap.count)
